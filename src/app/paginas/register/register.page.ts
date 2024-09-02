10e3c9edcbe32de3ca840674e21d851bdbf83cc8
Matelunaaaaa <benj.mateluna@duocuc.cl>
2024-09-01 21:35:13 -0400

    el registro funciona relativamente bien, falta ponerle alguna validacion al gmail, para que tenga el formato correcto

--- src/app/paginas/register/register.page.ts
+++ src/app/paginas/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  Usuario: string = "";
+  Contrasenna: string = "";
+  Gmail: string = "";
+  
+  constructor(public alerta: AlertController, private router: Router) {}
 
-  constructor() { }
+  async presentAlert(titulo: string, message: string) {
+    const alert = await this.alerta.create({
+      header: titulo,
+      message: message,
+      buttons: ["OK"]
+    });
+    await alert.present();
+  }
+
+  async mostrarAlerta() {
+    if (this.Usuario === "" || this.Contrasenna === "" || this.Gmail == "") {
+      await this.presentAlert("Advertencia", "Los campos no pueden estar vacíos");
+    } else {
+      this.redirigir();
+    }
+  }
+
+  redirigir() {
+    this.router.navigate(['login']);
+  }
 
   ngOnInit() {
   }
-
 }
