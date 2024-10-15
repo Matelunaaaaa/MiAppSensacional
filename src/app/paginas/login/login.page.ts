a7642f8c9ddbf5fffa5437fff7f7d4c07b3862e8
Matelunaaaaa <benj.mateluna@duocuc.cl>
2024-10-15 16:02:06 -0300

    Se agrego el fire base pero no funciona buenas noches

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { FirebaseLoginService } from 'src/app/servicios/firebase-login.service';
 import { InformacionusuarioService } from 'src/app/servicios/informacionusuario.service';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-login',
@@ -17,29 +20,51 @@ export class LoginPage implements OnInit {
   isUsuarioSuccess: boolean = false;
   isContrasennaSuccess: boolean = false;
 
-  constructor(private router: Router, private InformacionusuarioService: InformacionusuarioService) {}
+  constructor(public mensaje:ToastController, public alert:AlertController, private router: Router, private storage: Storage, private InformacionusuarioService: InformacionusuarioService, private acces:FirebaseLoginService) {}
+
+  async MensajeError(){
+    const alert = await this.alert.create({
+      header : 'Error de inicio de sesion',
+      subHeader: 'Contraseña o Usuario incorrecto',
+      message: 'Error al iniciar sesion en la cuenta',
+      buttons: ['Aceptar']
+    })
+  }
 
   /*se ve que los campos no esten vacios*/
-  checkInput() {
-    this.isUsuarioError = this.Usuario.trim() === '';
-    this.isUsuarioSuccess = !this.isUsuarioError;
-    
-    this.isContrasennaError = this.Contrasenna.trim() === '';
-    this.isContrasennaSuccess = !this.isContrasennaError;
-  }
+    checkInput() {
+      this.isUsuarioError = this.Usuario.trim() === '';
+      this.isUsuarioSuccess = !this.isUsuarioError;
 
-  /*funciona al presionarlos*/
-  mostrarAlerta() {
-    this.checkInput();
-    if (this.isUsuarioSuccess && this.isContrasennaSuccess) {
-      this.InformacionusuarioService.setUsername(this.Usuario);
-      this.redirigir();
+      this.isContrasennaError = this.Contrasenna.trim() === '';
+      this.isContrasennaSuccess = !this.isContrasennaError;
     }
+
+    /*funciona al presionarlos*/
+    mostrarAlerta() {
+      this.checkInput();
+      if (this.Usuario == "" && this.Contrasenna == "") {
+        console.log("No puede dejar ni el usuario ni la contraseña sin completar")
+        this.MensajeError()
+      }
+      else{
+        this.acces.login(this.Usuario,this.Contrasenna).then(()=>{
+          this.storage.set("nombre",this.Usuario)
+          this.storage.set("SessionID",true)
+          console.log("inicio de sesion exitoso ")
+          this.InformacionusuarioService.setUsername(this.Usuario);
+          this.redirigir();
+    }).catch(()=>{
+      this.MensajeError()
+    })
   }
+}
 
   redirigir() {
     this.router.navigate(['home']);
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.storage.create();
+  }
 }
