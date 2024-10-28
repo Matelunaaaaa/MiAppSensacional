2fb967939e89c42e20fbbdadbeb281b286ba4d52
Matelunaaaaa <benj.mateluna@duocuc.cl>
2024-10-27 21:45:51 -0300

    Control de error en la API si es que el usuario, no tiene conexion a internet

--- src/app/servicios/translation.service.ts
+++ src/app/servicios/translation.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, catchError, of } from 'rxjs';
 
 export interface TranslationResponse {
   responseData: {
@@ -25,7 +25,11 @@ export class TranslationService {
         console.log('Respuesta completa de la API:', response);
         const translatedText = response.responseData.translatedText;
         console.log('Texto traducido:', translatedText);
-        return translatedText; 
+        return translatedText;
+      }),
+      catchError(error => {
+        console.error('Error al conectar con la API:', error);
+        return of('Error: No se pudo conectar a la API, revisa tu conexion a internet');
       })
     );
   }
